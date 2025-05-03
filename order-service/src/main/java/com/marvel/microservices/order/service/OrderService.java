3fb3c3cca5c33d27a62525c11866d261ce139ddd
UtkarshAnand07 <utkarshanand7193@gmail.com>
2025-05-03 21:02:14 +0530

    order service

---
+++ order-service/src/main/java/com/marvel/microservices/order/service/OrderService.java
@@ -0,0 +1,29 @@
+package com.marvel.microservices.order.service;
+
+import com.marvel.microservices.order.dto.OrderRequest;
+import com.marvel.microservices.order.model.Order;
+import com.marvel.microservices.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    // map OrderRequest to Order object
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
+        order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
+
+        //save order to OrderRepository
+        orderRepository.save(order);
+    }
+}
