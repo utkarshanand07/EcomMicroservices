605ea26718348c2df87ca47350ac077e7ff63b86
UtkarshAnand07 <utkarshanand7193@gmail.com>
2025-05-29 20:06:47 +0530

    keycloak fix

--- api-gateway/src/main/java/com/marvel/microservices/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/marvel/microservices/gateway/config/SecurityConfig.java
@@ -17,8 +17,11 @@ public class SecurityConfig {
         return httpSecurity.authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(freeResourceUrls)
                         .permitAll()
-                        .anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+//                        .anyRequest().authenticated())
+//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+//                .build();
+
+                        .anyRequest().permitAll())
                 .build();
     }
 }
